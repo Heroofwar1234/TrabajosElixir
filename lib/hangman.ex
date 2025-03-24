a77d950cce12c1fdf2d189b289b8af385149e9fa
Joey <joeyalcocer@outlook.com>
2025-03-24 12:26:29 -0600

    Update code

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,18 +1,23 @@
 defmodule Hangman do
-  @moduledoc """
-  Documentation for `Hangman`.
-  """
+  def score_guess({word, correct, incorrect, 0}, _letter), do: {word, correct, incorrect, 0}
 
-  @doc """
-  Hello world.
+  def score_guess({word, correct, incorrect, guesses_left}, letter) do
+    cond do
+      String.contains?(word, letter) and not String.contains?(correct, letter) ->
+        {word, correct <> letter, incorrect, guesses_left}
 
-  ## Examples
+      not String.contains?(word, letter) and not String.contains?(incorrect, letter) ->
+        {word, correct, incorrect <> letter, guesses_left - 1}
 
-      iex> Hangman.hello()
-      :world
+      true ->
+        {word, correct, incorrect, guesses_left} # No change if letter was already guessed
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def format_feedback({word, correct, _incorrect, _guesses_left}) do
+    word
+    |> String.graphemes()
+    |> Enum.map(fn letter -> if String.contains?(correct, letter), do: letter, else: "-" end)
+    |> Enum.join()
   end
 end
